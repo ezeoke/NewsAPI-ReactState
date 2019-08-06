a1f37a95e16393b362daf9bd3fc51e0e7c5fb48e
cuba <ezeokecc@gmail.com>
2019-08-06 16:59:59 +0100

    i am consuming api with react state

---
+++ src/Components/API.js
@@ -0,0 +1,67 @@
+import React from "react";
+import countryCodes from "./countries";
+
+const Api_key = process.env.REACT_APP_API_KEY;
+
+class APICall extends React.Component {
+  state = {
+    dataSet: "",
+    value: "",
+    country: "NG"
+  };
+
+  async componentDidMount() {
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=${
+        this.state.country
+      }&apiKey=${Api_key}`
+    );
+    const data = await response.json();
+    console.log(data);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.country !== this.state.country) {
+      this.matchQuery();
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+
+  updateSearch = () => {
+    this.setState({
+      country: this.state.value
+    });
+  };
+
+  matchQuery = () => {
+    const objKey = Object.keys(countryCodes);
+    const objValue = Object.values(countryCodes);
+
+    const objData = objKey.filter(
+      item => item.toLowerCase() == this.state.country.toLowerCase()
+    );
+
+    console.log(objData);
+    console.log(objKey);
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.updateSearch}>search</button>
+      </div>
+    );
+  }
+}
+
+export default APICall;
