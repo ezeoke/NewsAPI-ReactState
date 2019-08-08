ed40010cf95e69ea14c71a55da0b1a7f6325c789
cuba <ezeokecc@gmail.com>
2019-08-08 16:50:10 +0100

    i added image gif to my Ui

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,28 +1,42 @@
 import React from "react";
 import contentStyles from "../styles/Content.module.css";
+import giphy from "./images/giphy.gif";
+import showMore from "./images/images.png";
 
 const Content = props => {
   const { content, loading } = props;
   console.log(content);
-
   return (
     <div>
-      {!loading
-        ? content.articles.map((item, index) => (
-            <div key={index}>
-              <h3>{item.title.toUpperCase()}</h3>
-              <img
-                // src={item.urlToImage}
-                alt="please visit news_api"
-                style={{ width: "70%", height: "40%" }}
-              />
-              <p>{item.description}</p>
+      {!loading ? (
+        content.articles.map((item, index) => (
+          <div key={index}>
+            <h3>{item.title.toUpperCase()}</h3>
+            <img
+              // src={item.urlToImage}
+              alt="please visit news_api"
+              style={{ width: "70%", height: "40%" }}
+            />
+            <p>
+              {item.description}{" "}
               <a href={item.url} target="blank">
-                ...full story
+                <img src={showMore} alt="" style={{ width: "3%" }} />
               </a>
-            </div>
-          ))
-        : "...loading"}
+            </p>
+          </div>
+        ))
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "75vh"
+          }}
+        >
+          <img src={giphy} alt="" />
+        </div>
+      )}
     </div>
   );
 };
